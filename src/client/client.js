f55b432bc9f186c67c467328562a0e3909fe9919
Kenichi Ukai <ken151102@gmail.com>
2019-05-01 16:56:17 +0900

    save until 30

--- src/client/client.js
+++ src/client/client.js
@@ -1,6 +1,13 @@
 // Startup point for the client side application
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './components/Home';
+// import Home from './components/Home'; Route内でHomeコンポーネントを読んでいるのでこちらだと不要
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './Routes';
 
-ReactDOM.hydrate(<Home />, document.querySelector('#root'));
+ReactDOM.hydrate(
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>,
+  document.querySelector('#root')
+);
