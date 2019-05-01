50b2e227c2a877198a31ca19ebafc4163730fb69
Kenichi Ukai <ken151102@gmail.com>
2019-05-01 12:02:35 +0900

    [test] update

--- build/bundle.js
+++ build/bundle.js
@@ -22629,7 +22629,7 @@ var Home = function Home() {
   return _react2.default.createElement(
     'div',
     null,
-    'I\'m the BEST BEST BEST home component'
+    'I\'m the home component'
   );
 };
 
